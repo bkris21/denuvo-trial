a349734e54b1da591faafa1ce3bda3fe1b082fa1
bkris21 <barczaykristof1993@gmail.com>
2024-10-05 15:14:49 +0200

    Correct n+1 problem correctly

--- project-customer-application/src/main/java/bkris/project_customer_application/repositories/CustomerRepository.java
+++ project-customer-application/src/main/java/bkris/project_customer_application/repositories/CustomerRepository.java
@@ -8,6 +8,6 @@ import org.springframework.data.repository.query.Param;
 
 public interface CustomerRepository extends JpaRepository<CustomerEntity, Long> {
 
-    @Query("select c from CustomerEntity c join fetch c.projects")
-    CustomerEntity findByNameAndContact(@Param("customer_name") String name, @Param("contact") String contact);
+    @Query("select c from CustomerEntity c join fetch c.projects where c.name =:customerName and c.contact = :contact")
+    CustomerEntity findByNameAndContact(@Param("customerName") String name, @Param("contact") String contact);
 }
