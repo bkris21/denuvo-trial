b8726f23af6559fb9434f7aaa0c0cbd0109dcea9
bkris21 <barczaykristof1993@gmail.com>
2024-10-05 11:13:22 +0200

    init

---
+++ project-customer-application/src/main/java/bkris/project_customer_application/ProjectCustomerApplication.java
@@ -0,0 +1,13 @@
+package bkris.project_customer_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCustomerApplication.class, args);
+	}
+
+}
---
+++ project-customer-application/src/test/java/bkris/project_customer_application/ProjectCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package bkris.project_customer_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
