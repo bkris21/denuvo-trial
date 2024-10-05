83932f1e8d19798161f5d2bf45ce8d480a17f8c4
bkris21 <barczaykristof1993@gmail.com>
2024-10-05 14:58:42 +0200

    Correct n+1 problem

--- project-customer-application/src/main/java/bkris/project_customer_application/repositories/CustomerRepository.java
+++ project-customer-application/src/main/java/bkris/project_customer_application/repositories/CustomerRepository.java
@@ -2,11 +2,13 @@ package bkris.project_customer_application.repositories;
 
 import bkris.project_customer_application.entities.CustomerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface CustomerRepository extends JpaRepository<CustomerEntity, Long> {
 
+    @Query("select c from CustomerEntity c join fetch c.projects")
     CustomerEntity findByNameAndContact(@Param("customer_name") String name, @Param("contact") String contact);
 }
